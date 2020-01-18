f8aaa4bf756a2762120e1a6c306257ba3507de89
Clément Guilbert <clement.guilbert@etu.udamail.fr>
2020-01-18 16:35:08 +0100

    Bang

---
+++ Grande Terreur Arctique VI/Godot/Bullet.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+var speed = 10
+var deadly = 0.0
+
+func _process(delta):
+    move(delta)
+    deadly+=delta
+    removeWhenOffScreen()
+
+func move(delta): 
+	#position = position + Vector2(cos(rotation_degrees), -sin(rotation_degrees)) * (50 * delta)
+	#$CollisionShape2D.position=position
+	position += speed * Vector2(1, 0).rotated(rotation)
+
+func removeWhenOffScreen():
+    if global_position.y < 0 or global_position.x < 0:
+        queue_free()
+
+func _on_Area2D_body_entered(body):
+	if(deadly>0.05):
+		if(body.has_method("eaten")):
+    	    body.eaten()
+		if(body.has_method("kill")):
+    	    body.kill()
---
+++ Grande Terreur Arctique VI/Godot/Zombie.gd
@@ -0,0 +1,41 @@
+extends KinematicBody2D
+
+const WEAPON = preload("res://Weapon.tscn")
+const MOVE_SPEED = 200
+
+var weapon
+
+onready var raycast = $RayCast2D
+
+var player = null
+
+func _ready():
+	add_to_group("zombies")
+	weapon = WEAPON.instance()
+	add_child(weapon)
+
+func _physics_process(delta):
+	if player == null:
+		return
+	
+	var vec_to_player = player.global_position - global_position
+	vec_to_player = vec_to_player.normalized()
+	global_rotation = atan2(vec_to_player.y, vec_to_player.x)
+	move_and_collide(vec_to_player * MOVE_SPEED * delta)
+	
+	if raycast.is_colliding():
+		var coll = raycast.get_collider()
+		if coll.name == "Player":
+			coll.kill()
+
+func eaten():
+	queue_free()
+	
+func _process(delta):
+    fire()
+
+func fire():
+	weapon.fire()
+
+func set_player(p):
+	player = p
