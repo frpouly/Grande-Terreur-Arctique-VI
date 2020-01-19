aa7ba27623752045307c73e9e85d203208c886dd
Clément Guilbert <clement.guilbert@etu.udamail.fr>
2020-01-19 03:53:31 +0100

    UI

--- ui/UI.gd
+++ ui/UI.gd
@@ -2,18 +2,13 @@ extends Control
 
 
 onready var popup = get_node("PopupPanel")
-onready var LifeBar = get_node("MarginContainer/VBoxContainer/Lifebar")
-onready var HeatLevel = get_node("MarginContainer/VBoxContainer/HeatLevel")
-onready var Hunger = get_node("MarginContainer/VBoxContainer/Hunger")
-
-
-func _ready():
-	pass
-
+onready var LifeBar = get_node("Lifebar")
+onready var HeatLevel = get_node("HeatLevel")
+onready var Hunger = get_node("Hunger")
+onready var timer = $PopupPanel/Timer
 
 func _on_Player_hit(health):
 	LifeBar.value=health
-
 
 func _on_Zombie_eaten():
 	$TextureProgress3.value+=1
@@ -22,19 +17,36 @@ func _on_Zombie_eaten():
 
 func _on_Player_hunger(hunger):
 	Hunger.value = hunger
-#	print(hunger)
+	print(hunger)
 
 
 func _on_Player_eaten():
 	Hunger.value+=20
 
+func _ready():
+	var text = $PopupPanel/Label
+	text.text = "Achievement Get ! \n Welcome to Ingluriuzz \n Stop au réchauffement climatique!"
+	popup.popup()
+	timer.start()
+	
 
 func _on_Bin_trasher():
 	var text = $PopupPanel/Label
 	text.text = "Achievement Get ! \n Bin Trasher \n Vous avez détruit une poubelle, jeune délinquant!"
 	popup.popup()
+	timer.start()
 	
 func _on_Wanted():
 	var text = $PopupPanel/Label
 	text.text = "Achievement Get ! \n Wanted \n Vous êtes plus poursuivi que la moulaga"
 	popup.popup()
+	timer.start()
+
+func _on_hunger():
+	var text = $PopupPanel/Label
+	text.text = "Achievement Get ! \n Au regime! \n Vous avez plus la dalle que Guilby avant un Gaosheng"
+	popup.popup()
+	timer.start()
+
+func _on_Timer_timeout():
+	popup.hide()
