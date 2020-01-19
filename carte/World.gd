e8887a58113d4bae59b0feaa9a08abddf71f34a1
François POULY <francois.pouly@etu.uca.fr>
2020-01-19 01:58:31 +0100

    Ajout des degats gun

--- carte/World.gd
+++ carte/World.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 onready var Zombies = get_node("Zombies")
 
-
 func _ready():
 	for zomb in Zombies.get_children():
 		zomb.connect("eaten", $Control, "_on_Zombie_eaten")
--- pickable/Rifle.gd
+++ pickable/Rifle.gd
@@ -7,13 +7,12 @@ extends "res://pickable/Weapon.gd"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_scale(Vector2(-1, 1))
-	reload_time = 0.1
-	bullets_total = 210
-	nb_bullets_magazine = 30
-	magazine_capacity = 30
-	damage = 10
+	reload_time = 3
+	bullets_total = 100
+	nb_bullets_magazine = 8
+	magazine_capacity = 8
+	damage = 100
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
